875b543da70b38a9b7b1e071b714620667b999d3
g-rashmi <rashmi.gupta7018@gmail.com>
2024-07-20 12:25:35 +0530

    completed

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -19,7 +19,6 @@ const wss = new ws_1.WebSocketServer({ server: httpserver });
 wss.on('connection', function connection(ws) {
     ws.on('error', console.error);
     const clientColor = getRandomColor();
-    ws.send(`color:${clientColor}`);
     ws.on('message', function message(data, isbinary) {
         wss.clients.forEach(function each(client) {
             if (client.readyState === ws_1.WebSocket.OPEN) {
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -15,7 +15,7 @@ const wss = new WebSocketServer({server:httpserver})
  wss. on('connection',function connection (ws){
 ws.on('error',console.error); 
 const clientColor = getRandomColor();
-ws.send(`color:${clientColor}`);
+
 ws.on('message',function message(data,isbinary){ 
   wss.clients.forEach(function each(client){
     if(client.readyState===WebSocket.OPEN){
