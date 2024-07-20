2ac28a323b393f209b0b26fe0801d2297168c871
g-rashmi <rashmi.gupta7018@gmail.com>
2024-07-20 12:28:48 +0530

    completedd

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -5,20 +5,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const ws_1 = require("ws");
-function getRandomColor() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
 const app = (0, express_1.default)();
 const httpserver = app.listen(300);
 const wss = new ws_1.WebSocketServer({ server: httpserver });
 wss.on('connection', function connection(ws) {
     ws.on('error', console.error);
-    const clientColor = getRandomColor();
     ws.on('message', function message(data, isbinary) {
         wss.clients.forEach(function each(client) {
             if (client.readyState === ws_1.WebSocket.OPEN) {
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,20 +1,13 @@
 import express from "express" 
 
 import {WebSocketServer,WebSocket} from "ws"; 
-function getRandomColor() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
+
 const app=express();  
 const httpserver=app.listen(300); 
 const wss = new WebSocketServer({server:httpserver}) 
  wss. on('connection',function connection (ws){
 ws.on('error',console.error); 
-const clientColor = getRandomColor();
+
 
 ws.on('message',function message(data,isbinary){ 
   wss.clients.forEach(function each(client){
